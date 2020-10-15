085dab0a04c65c4d6babee0d686cdb46273f4b59
markd <mark.permyakov.2002@bk.ru>
2020-10-15 23:51:56 +0300

    Простой цикл, в котором мы отслеживаем текущую страницу и текущую строчку на странице. Если искомое слово на ней есть, выводим номер страницы. Если строчек стало больше 45, начинаем новую страницу. Искомое слово записывается в файлике search_word.txt

--- src/text_index.py
+++ src/text_index.py
@@ -1,4 +1,21 @@
+current_page = 1
+page_line = 1
+current_line = 1
+total = 0
+with open('../data/search_word.txt', 'r', encoding='utf-8') as file:
+    search_word = file.readline()
+
+
 with open('../data/Childhood.txt', 'r', encoding='utf-8') as file:
     for line in file.readlines():
-        if line.find('подарки') != -1:
-            print(line.find('подарки'))
+        if line.find(search_word) != -1:
+            print(current_page)
+            total += 1
+        page_line += 1
+        if page_line == 46:
+            current_page += 1
+            page_line = 1
+if total == 0:
+    print("There is no word!")
+else:
+    print("There are", total, "words in the text")
