2e2caaa527d99240cff6856cc0f0533dc37dca94
markd <mark.permyakov.2002@bk.ru>
2020-10-18 21:37:43 +0300

    total превратился из числа в список, в котором хранятся количества вхождений того или иного слова в текст. Создал словарь, в котором ключи - элементы из total, а значения - соответсвующие слова, total которых подсчитан в списке. Все это для того, чтобы отсортировать total и спокойно вывести слова от самого популярного до менее попуярного.

--- src/text_index.py
+++ src/text_index.py
@@ -1,32 +1,49 @@
 current_page = 1
 page_line = 1
 current_line = 1
-total = 0
-number_of_words = 0
 
+number_of_word = 0
 
 list_of_words = list()
+total = list()
 
 with open('../data/search_words.txt', 'r', encoding='utf-8') as file:
     for line in file.readlines():
         list_of_words.append(line.strip())
+        total.append(0)
 
 
 with open('../data/Childhood.txt', 'r', encoding='utf-8') as file:
     for search_word in list_of_words:
+        print("Word "+"'"+search_word+"'"+":")
         for line in file.readlines():
             if line.find(search_word) != -1:
                 print(current_page)
                 print(line)
-                total += 1
+                total[number_of_word] += 1
             page_line += 1
             if page_line == 46:
                 current_page += 1
                 page_line = 1
-        if total == 0:
-            print("There is no word", "'" + search_word + "'","in the text")
-        else:
-            print("There are", total, "words", "'" + search_word + "'", "in the text")
+
         current_page = current_line = page_line = 1
-        total = 0
         file.seek(0)
+        number_of_word += 1
+
+
+totals_and_words = dict()
+for j in range(len(total)):
+    totals_and_words[total[j]] = list_of_words[j]
+
+
+total.sort()
+total.reverse()
+
+for i in range(len(total)):
+    if total[i] == 0:
+        print("There is no word", "'" + totals_and_words[i] + "'", "in the text")
+    else:
+        if total[i] == 1:
+            print("There is", total[i], "word", "'" + totals_and_words[total[i]] + "'", "in the text")
+        else:
+            print("There are", total[i], "words", "'" + totals_and_words[total[i]] + "'", "in the text")
