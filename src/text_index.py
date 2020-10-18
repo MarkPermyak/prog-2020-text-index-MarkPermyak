30243f31ea6057418b632c1040444cad1cac031c
markd <mark.permyakov.2002@bk.ru>
2020-10-18 21:58:58 +0300

    Цикла в цикле больше нет, появилась функция find_a_word, которая для каждого слова выводит страницу, на которой оно встретилось и соответсвующую строчку. Возникла проблема, что если записать в файл search_words.txt слово "лес", то программа зачтет слова "лестница", "прелесть" и т.д. Надо будет исправить

--- src/text_index.py
+++ src/text_index.py
@@ -1,34 +1,40 @@
-current_page = 1
-page_line = 1
-current_line = 1
-
-number_of_word = 0
-
 list_of_words = list()
 total = list()
 
+current_page = 1
+page_line = 1
+current_line = 1
+number_of_word = 0
 with open('../data/search_words.txt', 'r', encoding='utf-8') as file:
     for line in file.readlines():
         list_of_words.append(line.strip())
+        # решить вопрос с лес и лестница
         total.append(0)
+
+
+def find_a_word(word_to_find, initial_file):
+    global current_page
+    global page_line
+    global current_line
+    global number_of_word
+    for line in file.readlines():
+        if line.find(word_to_find) != -1:
+            print(current_page)
+            print(line)
+            total[number_of_word] += 1
+        page_line += 1
+        if page_line == 46:
+            current_page += 1
+            page_line = 1
+    current_page = current_line = page_line = 1
+    initial_file.seek(0)
+    number_of_word += 1
 
 
 with open('../data/Childhood.txt', 'r', encoding='utf-8') as file:
     for search_word in list_of_words:
         print("Word "+"'"+search_word+"'"+":")
-        for line in file.readlines():
-            if line.find(search_word) != -1:
-                print(current_page)
-                print(line)
-                total[number_of_word] += 1
-            page_line += 1
-            if page_line == 46:
-                current_page += 1
-                page_line = 1
-
-        current_page = current_line = page_line = 1
-        file.seek(0)
-        number_of_word += 1
+        find_a_word(search_word, file)
 
 
 totals_and_words = dict()
