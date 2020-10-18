c4ddf1b95cf3108f546d5145a930cf906bfcd27d
markd <mark.permyakov.2002@bk.ru>
2020-10-18 20:05:34 +0300

    Теперь можно искать сразу несколько слов по очереди. Для этого создал список слов list_of_words, в котором храню все искомые слова. Затем пробегаюсь по списку слов, ищу слово в тексте, обнуляю все переменные строк и страниц и иду дальше (снова циклы в цикле, но скоро пофиксим)

--- src/text_index.py
+++ src/text_index.py
@@ -2,20 +2,31 @@ current_page = 1
 page_line = 1
 current_line = 1
 total = 0
-with open('../data/search_word.txt', 'r', encoding='utf-8') as file:
-    search_word = file.readline()
+number_of_words = 0
+
+
+list_of_words = list()
+
+with open('../data/search_words.txt', 'r', encoding='utf-8') as file:
+    for line in file.readlines():
+        list_of_words.append(line.strip())
 
 
 with open('../data/Childhood.txt', 'r', encoding='utf-8') as file:
-    for line in file.readlines():
-        if line.find(search_word) != -1:
-            print(current_page)
-            total += 1
-        page_line += 1
-        if page_line == 46:
-            current_page += 1
-            page_line = 1
-if total == 0:
-    print("There is no word!")
-else:
-    print("There are", total, "words in the text")
+    for search_word in list_of_words:
+        for line in file.readlines():
+            if line.find(search_word) != -1:
+                print(current_page)
+                print(line)
+                total += 1
+            page_line += 1
+            if page_line == 46:
+                current_page += 1
+                page_line = 1
+        if total == 0:
+            print("There is no word", "'" + search_word + "'","in the text")
+        else:
+            print("There are", total, "words", "'" + search_word + "'", "in the text")
+        current_page = current_line = page_line = 1
+        total = 0
+        file.seek(0)
